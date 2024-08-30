26e8fc1a2b5f51dd2fd4911a2a1dfe35a4d7b3a3
Khlawy <mhmdalkhlawy45@gmail.com>
2024-08-30 18:10:50 +0300

    Finished Project 34, by Completing the CSS and JavaScript Files, and Added the Project Explanation File.

---
+++ Project 34 Animated Countdown/script.js
@@ -0,0 +1,52 @@
+const counter = document.querySelector('.counter')
+const finalMessage = document.querySelector('.final')
+const nums = document.querySelectorAll('.nums span')
+const inEl = document.querySelector('.in')
+const replay = document.querySelector('#replay')
+
+runAnimation()
+
+function resetDom() {
+    counter.classList.remove('hide')
+    finalMessage.classList.remove('show')
+
+    nums.forEach(num => 
+        num.classList.value = ''
+    )
+
+    nums[0].classList.add('in')
+}
+
+function runAnimation() {
+    nums.forEach((num, index) => {
+        const nextToLast = nums.length - 1
+
+        num.addEventListener('animationend', (e) => {
+            
+            if(e.animationName === 'goIn' && index !== nextToLast) {
+                num.classList.remove('in')
+                num.classList.add('out')
+            }
+
+            else if(e.animationName === 'goOut' && num.nextElementSibling) {
+                num.nextElementSibling.classList.add('in')
+            }
+
+            // This else if equal to the above one !!!!!!!!!
+
+            // else if(e.animationName === 'goOut' && index !== nextToLast) {
+            //     nums[index + 1].classList.add('in')
+            // }
+
+            else {
+                counter.classList.add('hide')
+                finalMessage.classList.add('show')
+            }
+        })
+    })
+}
+
+replay.addEventListener('click', () => {
+    resetDom()
+    runAnimation()
+})
